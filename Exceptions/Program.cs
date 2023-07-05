15b1006f5b6da0188875109cf3f2ca86bd47f7c8
Fatimə Qədirova <fatimagg@code.edu.az>
2023-07-05 17:22:38 +0400

    Exception

---
+++ Exceptions/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Exceptions
+{
+    using System;
+
+    public class ShortEmailException : Exception
+    {
+        public ShortEmailException(string message) : base(message)
+        {
+        }
+    }
+
+    public class NotAnEmailAdressException : Exception
+    {
+        public NotAnEmailAdressException(string message) : base(message)
+        {
+        }
+    }
+
+    public class EmailValidator
+    {
+        public bool Validate(string email)
+        {
+            if (email.Length < 10)
+            {
+                throw new ShortEmailException("Short email address.");
+            }
+
+            if (!email.EndsWith("@mail.com"))
+            {
+                throw new NotAnEmailAdressException("Not an email address.");
+            }
+
+            return true;
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var emailValidator = new EmailValidator();
+
+            try
+            {
+                bool isValid = emailValidator.Validate("fatima.qadirova@mail.com");
+                Console.WriteLine(isValid);
+            }
+            catch (ShortEmailException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (NotAnEmailAdressException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+
+    
+}
